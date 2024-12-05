2e4a62282529791795941c5eccd4b917c61e5016
remiltan123 <regiremiltan2002@gmail.com>
2024-12-05 08:29:31 -0800

    05/12

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,20 @@ const mysql = require('mysql2')
 
 const app = express();
 const port = process.env.port || 3001;
+
+app.use(express.json())
+app.use(cors())
+
+app.listen( port, (err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("App is running on "+ port)
+})
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'rege@1518',
+    database:'Tunion'
+})
